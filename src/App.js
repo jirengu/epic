0b7805794f34e27d7b318ec84e93dbef8668fa92
sun-shadow <sun-shadow@gmail.com>
2020-05-11 22:20:01 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Suspense, lazy } from 'react';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Loading from './components/Loading';
+
+import {
+  Switch,
+  Route
+} from 'react-router-dom';
+
+console.log(Loading)
+// import Home from './pages/Home';
+// import History from './pages/History';
+// import About from './pages/About';
+
+const Home = lazy(() => import('./pages/Home'));
+const History = lazy(() => import('./pages/History'));
+const About = lazy(() => import('./pages/About'));
+const Login = lazy(() => import('./pages/Login'));
+const Register = lazy(() => import('./pages/Register'));
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Suspense fallback={<Loading />}>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/history" component={History} />
+            <Route path="/about" component={About} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+          </Switch>
+        </Suspense>
+      </main>
+      <Footer />
+    </>
   );
 }
 
