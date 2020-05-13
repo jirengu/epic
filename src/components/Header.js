8aaaf8191733c2d57e2613435f0d2b2f1e5a4f23
sun-shadow <sun-shadow@gmail.com>
2020-05-13 20:55:41 +0800

    header引入antd

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LogoUrl from './logo.svg';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+import { Button } from 'antd';
 
 const Header = styled.header`
   display: flex;
@@ -28,27 +29,33 @@ const Login = styled.div`
   margin-left: auto;
 `;
 
-const Button = styled.button`
+const StyledButton = styled(Button)`
   margin-left: 10px;
 `;
 
 
 function Component() {
+
+  const [isLogin, setIsLogin] = useState(false);
+
   return (
     <Header>
-      <Logo src={LogoUrl}/> 
+      <Logo src={LogoUrl} />
       <nav>
         <StyledLink to="/" activeClassName="active" exact>首页</StyledLink>
         <StyledLink to="/history" activeClassName="active">上传历史</StyledLink>
         <StyledLink to="/about" activeClassName="active">关于我</StyledLink>
       </nav>
       <Login>
-        <Button>
-          <StyledLink to="/login">登录</StyledLink>
-        </Button>
-        <Button>
-          <StyledLink to="/register">注册</StyledLink>
-        </Button>
+        {
+          isLogin ? <>
+            饥人谷 <StyledButton type="primary" onClick={()=>setIsLogin(false)}>注销</StyledButton>
+          </> :<>
+          <StyledButton type="primary" onClick={()=>setIsLogin(true)}>登录</StyledButton>
+          <StyledButton type="primary">注册</StyledButton>
+          </>
+
+        }
       </Login>
 
     </Header>
