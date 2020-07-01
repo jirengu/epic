1ad43c7d10ffa73e073283c7e0254c5d1eb9c471
sun-shadow <sun-shadow@gmail.com>
2020-07-01 18:02:58 +0800

    init

---
+++ precache-manifest.a53575dd5c28683cd8c3ac9fa1a0db4d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3863ed0cd5823b266abcb582ef2a7dd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a19a32f87ea478251405",
+    "url": "./static/css/5.d7262850.chunk.css"
+  },
+  {
+    "revision": "27019826a5d2488eb2f2",
+    "url": "./static/css/main.c57a7da7.chunk.css"
+  },
+  {
+    "revision": "1e632157cddbe4db9704",
+    "url": "./static/js/0.afeb1254.chunk.js"
+  },
+  {
+    "revision": "5d811e32908312122185",
+    "url": "./static/js/1.64c17561.chunk.js"
+  },
+  {
+    "revision": "d47a5d7b0927dfdde75b",
+    "url": "./static/js/10.1da95351.chunk.js"
+  },
+  {
+    "revision": "d4fd1d35eeec9ddb64d6",
+    "url": "./static/js/11.afad8c73.chunk.js"
+  },
+  {
+    "revision": "6c4eccdf0ca447ae28cb",
+    "url": "./static/js/12.ca0316bc.chunk.js"
+  },
+  {
+    "revision": "5c74adc924a7467871e1",
+    "url": "./static/js/2.fcfdf78b.chunk.js"
+  },
+  {
+    "revision": "a19a32f87ea478251405",
+    "url": "./static/js/5.f3839e62.chunk.js"
+  },
+  {
+    "revision": "cd73b5aad7ae9f3669b4d74eb2d93cba",
+    "url": "./static/js/5.f3839e62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd70acfd9af5592649de",
+    "url": "./static/js/6.54c1b3ac.chunk.js"
+  },
+  {
+    "revision": "c4e7a211284d26470a54",
+    "url": "./static/js/7.4a2bc13d.chunk.js"
+  },
+  {
+    "revision": "b220166857464cfc5d3d",
+    "url": "./static/js/8.e3bc150e.chunk.js"
+  },
+  {
+    "revision": "b61d16e5f3f73390e2f6",
+    "url": "./static/js/9.9a913faf.chunk.js"
+  },
+  {
+    "revision": "27019826a5d2488eb2f2",
+    "url": "./static/js/main.60ebdec3.chunk.js"
+  },
+  {
+    "revision": "f18f64b5d446a4b22783",
+    "url": "./static/js/runtime-main.ddbc05ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ static/js/8.e3bc150e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-epic-teach"]=this["webpackJsonpreact-epic-teach"]||[]).push([[8],{358:function(e,t,a){"use strict";a.r(t);var c=a(0),n=a.n(c);t.default=function(){return n.a.createElement(n.a.Fragment,null,n.a.createElement("h1",null,"About"))}}}]);
+//# sourceMappingURL=8.e3bc150e.chunk.js.map
